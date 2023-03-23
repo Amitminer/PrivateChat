5275904c01a74bfe41ab3601c0d6f29af691497b
AmitxD <81667979+Amitminer@users.noreply.github.com>
2023-03-23 18:42:27 +0530

    Update PC.php

--- src/PC.php
+++ src/PC.php
@@ -10,263 +10,186 @@ use pocketmine\event\Listener;
 
 use pocketmine\event\player\PlayerChatEvent;
 
-use pocketmine\Player;
-
 use pocketmine\plugin\PluginBase;
 
-class PC extends PluginBase implements Listener {
-
-    private $privateChatEnabled = [];
-
-    private $privateChatMembers = [];
-
-    public function onEnable() : void {
-
-        $this->getLogger()->info("PrivateMine is enabled");
-
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-
-    }
-
-    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
-
-        if ($command->getName() === "pc") {
-
-            if (!isset($args[0])) {
-
-                return false;
-
-            }
-
-            switch ($args[0]) {
-
-                case "chat":
-
-                    if (!isset($args[1])) {
-
-                        return false;
-
-                    }
-
-                    if ($args[1] === "on") {
-
-                        $this->privateChatEnabled[$sender->getName()] = true;
-
-                        $sender->sendMessage("Private chat enabled.");
-
-                    } elseif ($args[1] === "off") {
-
-                        unset($this->privateChatEnabled[$sender->getName()]);
-
-                        $sender->sendMessage("Private chat disabled.");
-
-                    } else {
-
-                        return false;
-
-                    }
-
-                    break;
-
-                case "add":
-
-                    if (!isset($args[1])) {
-
-                        return false;
-
-                    }
-
-                    if (!isset($this->privateChatMembers[$sender->getName()])) {
-
-                        $this->privateChatMembers[$sender->getName()] = [];
-
-                    }
-
-                    array_push($this->privateChatMembers[$sender->getName()], strtolower($args[1]));
-
-                    break;
-
-                case "remove":
-
-                    if (!isset($args[1])) {
-
-                        return false;
-
-                    }
-
-                    if (isset($this->privateChatMembers[$sender->getName()])) {
-
-                        $index = array_search(strtolower($args[1]), $this->privateChatMembers[$sender->getName()]);
-
-                        if ($index !== false) {
-
-                            unset($this->privateChatMembers[$sender->getName()][$index]);
-
-                        }
-
-                    }
-
-                    break;
-
-                case "help":
-
-                    $sender->sendMessage("PrivateChat commands:\n/pc chat on/off - Enable/disable private chat\n/pc add playername - Add a player to your private chat group\n/pc remove playername - Remove a player from your private chat group\n/pc list - List members in your private chat group");
-
-                    break;
-
-                case "list":
-
-                    if (isset($this->privateChatMembers[$sender->getName()])) {
-
-                        $sender->sendMessage("Private chat members: " . implode(", ", $this->privateChatMembers[$sender->getName()]));
-
-                    } else {
-
-                        $sender->sendMessage("You have not added any members to your private chat.");
-
-                    }
-
-                    break;
-
-                default:
-
-                    return false;
-
-                }
-
-                return true;
-
-            }
-
-            return false;
-
-        }
-
-        public function onPlayerChat(PlayerChatEvent $event) {
-
-            $player = $event->getPlayer();
-
-            if (isset($this->privateChatEnabled[$player->getName()])) {
-
-                $event->setCancelled();
-
-                foreach ($this->getServer()->getOnlinePlayers() as $onlinePlayer) {
-
-                    if (in_array(strtolower($onlinePlayer->getName()), $this->privateChatMembers[$player->getName()])) {
-
-                        $onlinePlayer->sendMessage($player->getDisplayName() . " > " . $event->getMessage());
-
-                    }
-
-                }
-
-            }
-
-        }
-
-}
-<?php
-
-namespace AmitxD\PrivateChat;
-
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
-use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerChatEvent;
-use pocketmine\plugin\PluginBase;
 use pocketmine\player\Player;
 
 class PC extends PluginBase implements Listener {
 
     private $privateChatEnabled = [];
+
     private $privateChatMembers = [];
 
     public function onEnable() : void {
+
         $this->getLogger()->info("PrivateMine is enabled");
+
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
+
     }
 
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+
         if ($command->getName() === "pc") {
+
             if (!isset($args[0])) {
+
                 return false;
+
             }
+
             switch ($args[0]) {
+
                 case "chat":
+
                     if (!isset($args[1])) {
+
                         return false;
+
                     }
+
                     if ($args[1] === "on") {
+
                         $this->privateChatEnabled[$sender->getName()] = true;
+
                         $this->privateChatMembers[$sender->getName()][] = strtolower($sender->getName());
+
                         $sender->sendMessage("§l§d[PrivateChat] §aPrivate chat enabled.");
+
                     } elseif ($args[1] === "off") {
+
                         unset($this->privateChatEnabled[$sender->getName()]);
+
                         $sender->sendMessage("§l§d[PrivateChat] §cPrivate chat disabled.");
+
                     } else {
+
                         return false;
+
                     }
+
                     break;
+
                 case "add":
+
                     if (!isset($args[1])) {
+
                         return false;
+
                     }
+
                     $playerToAdd = strtolower($args[1]);
+
                     if (!isset($this->privateChatMembers[$sender->getName()])) {
+
                         $this->privateChatMembers[$sender->getName()] = [];
+
                     }
+
                     if (!in_array($playerToAdd, $this->privateChatMembers[$sender->getName()])) {
+
                         $this->privateChatMembers[$sender->getName()][] = $playerToAdd;
+
                         $sender->sendMessage("§d[PrivateChat] §aAdded $playerToAdd to your private chat group.");
+
                     } else {
+
                         $sender->sendMessage("§d[PrivateChat] §c$playerToAdd is already in your private chat group.");
+
                     }
+
                     break;
+
                 case "remove":
+
                     if (!isset($args[1])) {
+
                         return false;
+
                     }
+
                     if (isset($this->privateChatMembers[$sender->getName()])) {
+
                         $index = array_search(strtolower($args[1]), $this->privateChatMembers[$sender->getName()]);
+
                         if ($index !== false) {
+
                             $removedPlayer = $this->privateChatMembers[$sender->getName()][$index];
+
                             unset($this->privateChatMembers[$sender->getName()][$index]);
+
                             $sender->sendMessage("§d[PrivateChat] §r §e{$removedPlayer}§r has been removed from your private chat group.");
+
                         } else {
+
                             $sender->sendMessage("§d[PrivateChat] §e{$args[1]}§r is not in your private chat group.");
+
                         }
+
                     } else {
+
                         $sender->sendMessage("§d[PrivateChat] §c You have not added any members to your private chat.");
+
                     }
+
                     break;
+
                 case "help":
+
                     $sender->sendMessage("§aPrivateChat commands:\n§b/pc chat on/off§r - Enable/disable private chat\n§b/pc add playername§r - Add a player to your private chat group\n§b/pc remove playername§r - Remove a player from your private chat group\n§b/pc list§r - List members in your private chat group");
+
                     break;
+
                 case "list":
+
                     if (isset($this->privateChatMembers[$sender->getName()])) {
+
                         $sender->sendMessage("§d[PrivateChat] §aPrivate chat members:\n§c " . implode(", ", $this->privateChatMembers[$sender->getName()]));
+
                     } else {
+
                         $sender->sendMessage("§d[PrivateChat] §cYou have not added any members to your private chat.");
+
                     }
+
                     break;
+
                 default:
+
                     return false;
+
                 }
+
                 return true;
+
             }
+
             return false;
+
         }
 
         public function onPlayerChat(PlayerChatEvent $event) {
+
             $player = $event->getPlayer();
+
             if (isset($this->privateChatEnabled[$player->getName()])) {
+
                 $event->cancel();
+
                 foreach ($this->getServer()->getOnlinePlayers() as $onlinePlayer) {
+
                     $senderName = $player->getName();
+
                     if (isset($this->privateChatMembers[$senderName]) && in_array(strtolower($onlinePlayer->getName()), $this->privateChatMembers[$senderName])) {
+
                         $onlinePlayer->sendMessage("§b[PrivateChat] §a" . $player->getDisplayName() . " §r> " . $event->getMessage());
+
                     }
+
                 }
+
             }
+
         }
+
 }
